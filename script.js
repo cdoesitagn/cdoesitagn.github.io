4324baf7eaef68b106c54fe3429ffab4adf2d634
Duc Nguyen <ncmduc.working@gmail.com>
2024-11-22 14:03:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Placeholder for interactivity or analytics
+console.log("Game Portal Initialized");
+
+// Integrate Google Analytics (replace with your tracking code)
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "YOUR_GOOGLE_ANALYTICS_ID");
