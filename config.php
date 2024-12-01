ae72e2075fe05ddb91016ea16908863af26d242f
Duc Nguyen <ncmduc.working@gmail.com>
2024-12-01 16:34:29 +0700

    container-reedit

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 // MySQL Database User
-define("DB_USERNAME", "user");
+define("DB_USERNAME", "root");
 // MySQL Database Password
-define("DB_PASSWORD", "user");
+define("DB_PASSWORD", "");
 // MySQL Hostname
-define("DB_HOST", "ACENSOR");
+define("DB_HOST", "localhost");
 // MySQL Database Name
 define("DB_NAME", "game_portal");
 
 // Site URL
-$site_url = "http://localhost/html5-portal-main/"; // e.g (http://example.com)
+$site_url = "http://localhost/cdoesitagn.github.io/"; // e.g (http://example.com)
 ?>
